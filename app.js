04639321e7a8f50f7e74d872e34a30d64d872871
Nikita Kukshinski <nkstatedev@gmail.com>
2020-11-15 23:34:24 -0600

    Import EJS and create ejs files

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
 const app = express();
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
